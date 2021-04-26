0e577a1b63fa9b4dada863d745c7a05005deb7ec
BarathNallusamy <nallu.baray@gmail.com>
2021-04-26 13:50:51 +0100

    Initial commit

---
+++ SerialisationApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerialisationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
