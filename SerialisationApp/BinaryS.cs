4cf1812631987c50554547460d3881fe62674937
BarathNallusamy <nallu.baray@gmail.com>
2021-04-26 16:28:24 +0100

    Json serialization

---
+++ SerialisationApp/BinaryS.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.IO;
+//using System.Runtime.Serialization.Formatters.Binary;
+
+//namespace SerialisationApp
+//{
+//    class BinaryS
+//    {
+//        static readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+//        static void Main(string[] args)
+//        {
+//            //create a trainee
+//            Trainee t1 = new Trainee { FirstName = "Barath", LastName = "Nallusamy", SpartaNo = 101 };
+//            SerializeToFileBinary(t1);
+
+//            Trainee t2 = DeSerializeFromFileBinary("BinaryTrainee.bin");
+
+//        }
+
+//        private static Trainee DeSerializeFromFileBinary(string fileName)
+//        {
+//            //create a file path
+//            var filePath = $"{_path}/{fileName}";
+
+//            //create a stream for reading
+//            Stream file = File.OpenRead(filePath);
+
+//            //create the binary formatter and use it to read the file
+//            BinaryFormatter reader = new BinaryFormatter();
+//            var trainee = (Trainee)reader.Deserialize(file);
+//            file.Close();
+//            return trainee;
+//        }
+
+//        private static void SerializeToFileBinary(object o)
+//        {
+//            //create the file path
+//            var filePath = $"{_path}/BinaryTrainee.bin";
+
+//            //create a file stream object
+//            FileStream file = File.Create(filePath);
+
+//            //create a binary formatter object, and use it to serialize the trainee object to file
+//            BinaryFormatter writer = new BinaryFormatter();
+//            writer.Serialize(file, o);
+//            file.Close();
+//        }
+//    }
+//}
---
+++ SerialisationApp/JSON_S.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerialisationApp
+{
+    class JSON_S
+    {
+        static readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        static void Main(string[] args)
+        {
+            //create a trainee
+            Trainee t1 = new Trainee { FirstName = "Barath", LastName = "Nallusamy", SpartaNo = 101 };
+            SerializeToFileJSON(t1);
+
+            //Trainee t2 = DeSerializeFromFileJSON("101 - Barath Nallusamy.json");
+
+            //create a course
+            Course eng83 = new Course { Title = "Engineering 83", Subject = "C# SDET", StartDate = new DateTime(2021, 3, 16) };
+            eng83.AddTrainee(t1);
+            eng83.AddTrainee(new Trainee { FirstName = "Martin", LastName = "Beard", SpartaNo = 102 });
+            SerializeToFileJSON(eng83);
+
+            Course otherCourse = DeSerializeCourseFromFileJSON("NewCourse.json");
+        }
+
+        private static Trainee DeSerializeFromFileJSON(string fileName)
+        {
+            //create a file path
+            var filePath = $"{_path}/{fileName}";
+            string jsonString = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Trainee>(jsonString);
+        }
+
+        private static Course DeSerializeCourseFromFileJSON(string fileName)
+        {
+            //create a file path
+            var filePath = $"{_path}/{fileName}";
+            string jsonString = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Course>(jsonString);
+        }
+
+        private static void SerializeToFileJSON(object o)
+        {
+            //create the file path
+            var filePath = $"{_path}/{o}.json";
+            string jsonString = JsonConvert.SerializeObject(o);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
